07a4d36ebfc6cc9dc20ef770d6aa9933b7eacb49
Paulo Wender <opaulowender@gmail.com>
2023-10-19 10:15:08 -0400

    Melhorias no log

--- Logger.py
+++ Logger.py
@@ -3,12 +3,12 @@ import os
 LOG_FILE = "log.txt"
 
 
-def saveLog(log: str):
-    if not os.path.exists(LOG_FILE):
-        with open(LOG_FILE, "w") as f:
-            f.write("")
+def saveLog(log: str, file=LOG_FILE):
+    if not os.path.exists(file):
+        with open(file, "w") as f:
+            f.write(log)
     else:
-        with open(LOG_FILE, "a") as f:
+        with open(file, "a") as f:
             f.write("\n" + log)
 
     return True
